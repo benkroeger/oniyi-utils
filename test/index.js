edfc6255295f02bf31db20121c7d46024d5ceb34
Benjamin Kroeger <benjamin.kroeger@gmail.com>
2017-02-10 08:00:43 +0100

    updates dev-dependencies

--- test/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import test from 'ava';
-import oniyiUtils from '../lib/index.js';
-
-test('awesome:test', t => {
-  const message = 'everything is awesome';
-  t.is(oniyiUtils('awesome'), message, message);
-});
