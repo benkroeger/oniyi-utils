ad9ee41aa95bf892a5f360b6d4f5647febb7c94a
Benjamin Kroeger <benjamin.kroeger@gmail.com>
2017-02-10 08:00:05 +0100

    adds module-loaders:resolve()
    new method to load modules from custom paths and with potential prefixes

---
+++ test/fixtures/src-dir1/oniyi-logger.js
@@ -0,0 +1,1 @@
+module.exports = 'oniyi-logger';
---
+++ test/module-loaders.test.js
@@ -0,0 +1,56 @@
+'use strict';
+
+import path from 'path';
+import test from 'ava';
+import resolve from '../lib/module-loaders.js';
+
+test('fails if "name" is empty', (t) => {
+  const fn = () => { resolve({}); };
+  t.throws(fn, TypeError, 'should fail when invoked without name parameter');
+});
+
+test('fails if "name" can not be resolved', (t) => {
+  const fn = () => { resolve({ name: 'does-not-exist' }); };
+  t.throws(fn, Error, 'should fail when invoked without name parameter');
+});
+
+test('resolves "oniyi-logger" module from node_modules', (t) => {
+  // eslint-disable-next-line global-require
+  const expected = require('oniyi-logger');
+
+  const result = resolve({ name: 'oniyi-logger', prefixes: ['prefix-1-'] });
+  t.is(result, expected);
+});
+
+test('resolves "foo" module from src-dir1', (t) => {
+  // eslint-disable-next-line global-require
+  const expected = require('./fixtures/src-dir1/foo');
+
+  const paths = ['fixtures/src-dir1', 'fixtures/src-dir2']
+    .map(dir => path.resolve(__dirname, dir));
+
+  const result = resolve({ name: 'foo', paths, prefixes: ['prefix-1-'] });
+  t.is(result, expected);
+});
+
+test('resolves "bar" module from src-dir2', (t) => {
+  // eslint-disable-next-line global-require
+  const expected = require('./fixtures/src-dir2/bar');
+
+  const paths = ['fixtures/src-dir1', 'fixtures/src-dir2']
+    .map(dir => path.resolve(__dirname, dir));
+
+  const result = resolve({ name: 'bar', paths, prefixes: ['prefix-1-'] });
+  t.is(result, expected);
+});
+
+test('resolves "oniyi-logger" module from src-dir1', (t) => {
+  // eslint-disable-next-line global-require
+  const expected = require('./fixtures/src-dir1/oniyi-logger');
+
+  const paths = ['fixtures/src-dir1', 'fixtures/src-dir2']
+    .map(dir => path.resolve(__dirname, dir));
+
+  const result = resolve({ name: 'oniyi-logger', paths, prefixes: ['prefix-1-'] });
+  t.is(result, expected);
+});
